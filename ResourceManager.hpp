9ab9c765fe4b2051cfb65481828918f7e09b8a55
chimek2107 <b.chimkowski@tuta.io>
2022-11-04 18:30:30 +0100

    a

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,19 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+	Resource* r1;
+
+public:
+
+	ResourceManager() { r1 = new Resource{} }
+
+	double get() {return r1.get() }
+
+	ResourceManager(const ResourceManager& l) = default; 
+
+	~ResourceManager() { delete r1; }
+
+	ResourceManager& operator=(const ResourceManager& t) { /* ... */ return *this; }
+
 };
